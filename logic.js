beeed9a648054505d766e2f4aeb88f39435af46d
kyw <ingngnin@gmail.com>
2022-08-20 00:58:42 +0900

    HTTP와 HTTPS의 차이점 때문에 안되긴 할텐데 일단 테스트 해보자.

--- logic.js
+++ logic.js
@@ -18,21 +18,23 @@ function init() {
   updateWord()
     .then((response) => response.json())
     .then((data) => {
-      TODAY_WORDLE = data.result.toUpperCase();
-      drawBoard();
-      setEventListeners();
+      TODAY_WORDLE = data.result.toUpperCase()
+      drawBoard()
+      setEventListeners()
+      console.log(TODAY_WORDLE)
     })
     .catch(() => {
-      drawBoard();
-      setEventListeners();
-    });
+      drawBoard()
+      setEventListeners()
+      console.log("CATCH")
+    })
 }
 
 //게임화면 초기화
 function drawBoard() {
-  const BOARD_ROW = 6;
-  const board = document.querySelector(".board");
-  board.innerHTML = "";
+  const BOARD_ROW = 6
+  const board = document.querySelector(".board")
+  board.innerHTML = ""
   const makeRow = function (rowNumber) {
     return `
     <section class="row${rowNumber}">
@@ -42,146 +44,146 @@ function drawBoard() {
         <button disabled="disabled">&nbsp;</button>
         <button disabled="disabled">&nbsp;</button>
       </section>
-    `;
-  };
+    `
+  }
   for (let index = 0; index < BOARD_ROW; index++) {
-    board.innerHTML += makeRow(index + 1);
+    board.innerHTML += makeRow(index + 1)
   }
 }
 
 //이벤트 리스너 등록
 function setEventListeners() {
-  registKeyEvent();
-  registRestartBtn();
+  registKeyEvent()
+  registRestartBtn()
 }
 
 //다시하기 버튼 등록
 function registRestartBtn() {
-  const restartBtn = document.querySelector(".btn_close");
+  const restartBtn = document.querySelector(".btn_close")
   restartBtn.addEventListener("click", () => {
-    restart();
-  });
+    restart()
+  })
 }
 
 //키 입력 이벤트 등록
 function registKeyEvent() {
-  const dq = (element) => document.querySelector(element);
-  const body = dq("body");
-  const board = dq(".board");
+  const dq = (element) => document.querySelector(element)
+  const body = dq("body")
+  const board = dq(".board")
   body.addEventListener("keydown", (event) => {
     if (isGameOver) {
-      return;
+      return
     }
-    const curRow = board.querySelectorAll(`.row${CUR_ROW} button`);
+    const curRow = board.querySelectorAll(`.row${CUR_ROW} button`)
 
     switch (event.key) {
       case "Backspace":
-        pushBackspace();
-        break;
+        pushBackspace()
+        break
       case "Enter":
-        pushEnter();
-        break;
+        pushEnter()
+        break
       default:
-        pushAlphabet();
-        break;
+        pushAlphabet()
+        break
     }
 
     function pushBackspace() {
       if (CUR_COLUMN > 5) {
-        CUR_COLUMN = 5;
+        CUR_COLUMN = 5
       } else if (CUR_COLUMN < 1) {
-        CUR_COLUMN = 1;
+        CUR_COLUMN = 1
       }
-      const curBtn = curRow[CUR_COLUMN - 1];
-      curBtn.innerHTML = "&nbsp";
-      CUR_COLUMN--;
+      const curBtn = curRow[CUR_COLUMN - 1]
+      curBtn.innerHTML = "&nbsp"
+      CUR_COLUMN--
     }
 
     function pushEnter() {
       if (CUR_COLUMN < 5) {
-        return;
+        return
       }
 
       if (checkWord(curRow)) {
-        viewResult("Success.", "inline");
-        isSuccess = true;
-        isGameOver = true;
+        viewResult("Success.", "inline")
+        isSuccess = true
+        isGameOver = true
       }
 
-      CUR_ROW++;
-      CUR_COLUMN = 0;
+      CUR_ROW++
+      CUR_COLUMN = 0
       if (CUR_ROW > 6) {
         if (isSuccess) {
-          return;
+          return
         }
-        viewResult("Fail..", "inline");
-        isGameOver = true;
+        viewResult("Fail..", "inline")
+        isGameOver = true
       }
     }
 
     function pushAlphabet() {
-      const checkKey = /^[a-zA-Z]$/gim;
+      const checkKey = /^[a-zA-Z]$/gim
       if (checkKey.test(event.key)) {
         if (CUR_COLUMN > 4) {
-          return;
+          return
         } else if (CUR_COLUMN < 0) {
-          CUR_COLUMN = 0;
+          CUR_COLUMN = 0
         }
-        const curBtn = curRow[CUR_COLUMN];
-        curBtn.textContent = event.key.toUpperCase();
-        CUR_COLUMN++;
+        const curBtn = curRow[CUR_COLUMN]
+        curBtn.textContent = event.key.toUpperCase()
+        CUR_COLUMN++
       }
     }
-  });
+  })
 }
 
 //입력한 글자 검사.
 function checkWord(curRow) {
-  let arrTodayWord = [...TODAY_WORDLE];
-  let arrInputWord = [];
-  let isPerfect = false;
+  let arrTodayWord = [...TODAY_WORDLE]
+  let arrInputWord = []
+  let isPerfect = false
   curRow.forEach((element) => {
-    arrInputWord.push(element.textContent);
-  });
+    arrInputWord.push(element.textContent)
+  })
 
-  isPerfect = checkMatch(arrInputWord, arrTodayWord, curRow);
-  checkExist(arrInputWord, arrTodayWord, curRow);
-  return isPerfect;
+  isPerfect = checkMatch(arrInputWord, arrTodayWord, curRow)
+  checkExist(arrInputWord, arrTodayWord, curRow)
+  return isPerfect
 }
 
 //자리와 글자 모두 일치 검사
 function checkMatch(input, today, cur) {
-  let MATCH_BG_COLOR = "#538D4E";
-  let isAnswer = true;
+  let MATCH_BG_COLOR = "#538D4E"
+  let isAnswer = true
   //match
   for (let i = 0; i < input.length; i++) {
     if (today[i] == input[i]) {
-      cur[i].style.backgroundColor = MATCH_BG_COLOR;
-      today[i] = "";
-      input[i] = "";
-      continue;
+      cur[i].style.backgroundColor = MATCH_BG_COLOR
+      today[i] = ""
+      input[i] = ""
+      continue
     }
-    isAnswer = false;
+    isAnswer = false
   }
 
-  return isAnswer;
+  return isAnswer
 }
 
 //자리 일치 검사
 function checkExist(input, today, cur) {
-  let EXIST_BG_COLOR = "#B59F3B";
+  let EXIST_BG_COLOR = "#B59F3B"
   //match
   for (let i = 0; i < input.length; i++) {
     for (let j = 0; j < today.length; j++) {
       if (input[i] == "") {
-        break;
+        break
       }
 
       if (input[i] == today[j]) {
-        cur[i].style.backgroundColor = EXIST_BG_COLOR;
-        today[j] = "";
-        input[i] = "";
-        break;
+        cur[i].style.backgroundColor = EXIST_BG_COLOR
+        today[j] = ""
+        input[i] = ""
+        break
       }
     }
   }
@@ -190,28 +192,28 @@ function checkExist(input, today, cur) {
 //결과 화면 출력
 function viewResult(message, visible) {
   setTimeout(() => {
-    document.querySelector("#pop_info_1").style.display = visible;
-    document.querySelector(".result").textContent = message;
-    document.querySelector(".wordle").textContent = `[ ${TODAY_WORDLE} ]`;
-  }, 500);
+    document.querySelector("#pop_info_1").style.display = visible
+    document.querySelector(".result").textContent = message
+    document.querySelector(".wordle").textContent = `[ ${TODAY_WORDLE} ]`
+  }, 500)
 }
 
 //게임 데이터 초기화
 function restart() {
-  document.querySelector("#pop_info_1").style.display = "none";
-  CUR_ROW = 1;
-  CUR_COLUMN = 0;
-  isGameOver = false;
-  isSuccess = false;
+  document.querySelector("#pop_info_1").style.display = "none"
+  CUR_ROW = 1
+  CUR_COLUMN = 0
+  isGameOver = false
+  isSuccess = false
   //updateWord();
-  drawBoard();
+  drawBoard()
 }
 
 //서버에 요청해서 단어 받아오기
 //당연하지만 서버가 켜져있어야하고, 요청을 처리 할 수 있어야 한다.
 function updateWord() {
   // return fetch("http://localhost:8080/wordle/wod/get-wordle");
-  return fetch("http://tomcattest-env.eba-hidxcrst.us-west-1.elasticbeanstalk.com/index.jsp");
+  return fetch("http://wordle-env.eba-qmhxwgzr.ap-northeast-2.elasticbeanstalk.com/wod/test.do")
 }
 // function updateWord() {
 //   return fetch("http://localhost:8080/wordle/wod/get-wordle");
