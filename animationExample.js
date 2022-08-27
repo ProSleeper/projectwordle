041e09d0e1c258dfb8a1afbc8dd59613d00f49f7
kyw <ingngnin@gmail.com>
2022-08-28 02:46:15 +0900

    btn 각각 도는 애니메이션 성공
    남은 개발 사항
    1. 애니메이션 중간에 글자 배경 바뀌게 변경
    2. 애니메이션 중에는 어떤 입력도 받지 못하게 변경
    3. 그 외 너무 많아서 나열도 힘듬

---
+++ animationExample.js
@@ -0,0 +1,41 @@
+//이거 참고해서 내 애니메이션 코드 부분도 바꾸면 될듯.
+//애니메이션을 이해한다기보다 비동기 코드를 제대로 처리하는 방법을 배우는 거라서 필수적으로 하면 더더욱 좋을 것 같다.
+var KEYFRAMES = {
+  o: [{ opacity: 1 }, { opacity: 0 }, { opacity: 1 }],
+  s: [{ transform: "scale(1)" }, { transform: "scale(1.4)" }, { transform: "scale(1)" }],
+  r: [{ transform: "rotate(0deg)" }, { transform: "rotate(200deg)" }, { transform: "rotate(0deg)" }],
+  bb: [{ background: "#4598b6" }, { background: "#444444" }, { background: "#4598b6" }],
+  br: [{ background: "#b64598" }, { background: "#444444" }, { background: "#b64598" }],
+};
+
+
+//콜백방식
+var box = document.querySelector(".callback");
+var anim1 = box.animate(KEYFRAMES.o, 1000);
+anim1.onfinish = function () {
+  var anim2 = box.animate(KEYFRAMES.t, 1000);
+  anim2.onfinish = function () {
+    var anim3 = box.animate(KEYFRAMES.r, 1000);
+    anim3.onfinish = function () {
+      box.animate(KEYFRAMES.bb, 1000);
+    };
+  };
+};
+
+
+//프로미스 방식
+var box2 = document.querySelector(".promise");
+var animA = box2.animate(KEYFRAMES.o, 1000);
+animA.finished
+  .then(function () {
+    var animB = box2.animate(KEYFRAMES.t, 1000);
+    return animB.finished;
+  })
+  .then(function () {
+    var animC = box2.animate(KEYFRAMES.r, 1000);
+    return animC.finished;
+  })
+  .then(function () {
+    box2.animate(KEYFRAMES.br, 1000);
+  });
+
--- logic.js
+++ logic.js
@@ -1,4 +1,6 @@
 "use strict";
+import * as keyAnim from "./animation.js";
+
 window.addEventListener("load", () => {
   init();
 });
@@ -18,22 +20,22 @@ function init() {
   updateWord()
     .then((response) => response.json())
     .then((data) => {
-      TODAY_WORDLE = data.result.toUpperCase()
+      TODAY_WORDLE = data.result.toUpperCase();
     })
     .catch(() => {
-      console.error("request Fail")
+      console.error("request Fail");
     })
     .finally(() => {
-      drawBoard()
-      setEventListeners()
-    })
+      drawBoard();
+      setEventListeners();
+    });
 }
 
 //게임화면 초기화
 function drawBoard() {
-  const BOARD_ROW = 6
-  const board = document.querySelector(".board")
-  board.innerHTML = ""
+  const BOARD_ROW = 6;
+  const board = document.querySelector(".board");
+  board.innerHTML = "";
   const makeRow = function (rowNumber) {
     return `
     <section class="row${rowNumber}">
@@ -43,150 +45,152 @@ function drawBoard() {
         <button disabled="disabled">&nbsp;</button>
         <button disabled="disabled">&nbsp;</button>
       </section>
-    `
-  }
+    `;
+  };
   for (let index = 0; index < BOARD_ROW; index++) {
-    board.innerHTML += makeRow(index + 1)
+    board.innerHTML += makeRow(index + 1);
   }
 }
 
 //이벤트 리스너 등록
 function setEventListeners() {
-  registKeyEvent()
-  registRestartBtn()
+  registKeyEvent();
+  registRestartBtn();
 }
 
 //다시하기 버튼 등록
 function registRestartBtn() {
-  const restartBtn = document.querySelector(".btn_close")
+  const restartBtn = document.querySelector(".btn_close");
   restartBtn.addEventListener("click", () => {
-    restart()
-  })
+    restart();
+  });
 }
 
 //키 입력 이벤트 등록
 function registKeyEvent() {
-  const dq = (element) => document.querySelector(element)
-  const body = dq("body")
-  const board = dq(".board")
+  const dq = (element) => document.querySelector(element);
+  const body = dq("body");
+  const board = dq(".board");
   body.addEventListener("keydown", (event) => {
     if (isGameOver) {
-      return
+      return;
     }
-    const curRow = board.querySelectorAll(`.row${CUR_ROW} button`)
+    const curRow = board.querySelectorAll(`.row${CUR_ROW} button`);
 
     switch (event.key) {
       case "Backspace":
-        pushBackspace()
-        break
+        pushBackspace();
+        break;
       case "Enter":
-        pushEnter()
-        break
+        pushEnter();
+        break;
       default:
-        pushAlphabet()
-        break
+        pushAlphabet();
+        break;
     }
 
     function pushBackspace() {
       if (CUR_COLUMN > 5) {
-        CUR_COLUMN = 5
+        CUR_COLUMN = 5;
       } else if (CUR_COLUMN < 1) {
-        CUR_COLUMN = 1
+        CUR_COLUMN = 1;
       }
-      const curBtn = curRow[CUR_COLUMN - 1]
-      curBtn.innerHTML = "&nbsp"
-      CUR_COLUMN--
+      const curBtn = curRow[CUR_COLUMN - 1];
+      curBtn.innerHTML = "&nbsp";
+      CUR_COLUMN--;
     }
 
-    function pushEnter() {
+    async function pushEnter() {
       if (CUR_COLUMN < 5) {
-        return
+        return;
       }
+
+      await keyAnim.animationRow(curRow).then(() => console.log("모든 btn 애니메이션 종료 후 실행 됨"));
 
       if (checkWord(curRow)) {
-        viewResult("Success.", "inline")
-        isSuccess = true
-        isGameOver = true
+        viewResult("Success.", "inline");
+        isSuccess = true;
+        isGameOver = true;
       }
 
-      CUR_ROW++
-      CUR_COLUMN = 0
+      CUR_ROW++;
+      CUR_COLUMN = 0;
       if (CUR_ROW > 6) {
         if (isSuccess) {
-          return
+          return;
         }
-        viewResult("Fail..", "inline")
-        isGameOver = true
+        viewResult("Fail..", "inline");
+        isGameOver = true;
       }
     }
 
     function pushAlphabet() {
-      const checkKey = /^[a-zA-Z]$/gim
+      const checkKey = /^[a-zA-Z]$/gim;
       if (checkKey.test(event.key)) {
         //animation 부분?!
         if (CUR_COLUMN > 4) {
-          return
+          return;
         } else if (CUR_COLUMN < 0) {
-          CUR_COLUMN = 0
+          CUR_COLUMN = 0;
         }
-        const curBtn = curRow[CUR_COLUMN]
-        
-        curBtn.animate(KEYFRAMES.s, 100);
-        
-        curBtn.textContent = event.key.toUpperCase()
-        CUR_COLUMN++
+        const curBtn = curRow[CUR_COLUMN];
+
+        curBtn.animate(keyAnim.KEYFRAMES.s, 100);
+
+        curBtn.textContent = event.key.toUpperCase();
+        CUR_COLUMN++;
       }
     }
-  })
+  });
 }
 
 //입력한 글자 검사.
 function checkWord(curRow) {
-  let arrTodayWord = [...TODAY_WORDLE]
-  let arrInputWord = []
-  let isPerfect = false
+  let arrTodayWord = [...TODAY_WORDLE];
+  let arrInputWord = [];
+  let isPerfect = false;
   curRow.forEach((element) => {
-    arrInputWord.push(element.textContent)
-  })
+    arrInputWord.push(element.textContent);
+  });
 
-  isPerfect = checkMatch(arrInputWord, arrTodayWord, curRow)
-  checkExist(arrInputWord, arrTodayWord, curRow)
-  return isPerfect
+  isPerfect = checkMatch(arrInputWord, arrTodayWord, curRow);
+  checkExist(arrInputWord, arrTodayWord, curRow);
+  return isPerfect;
 }
 
 //자리와 글자 모두 일치 검사
 function checkMatch(input, today, cur) {
-  let MATCH_BG_COLOR = "#538D4E"
-  let isAnswer = true
+  let MATCH_BG_COLOR = "#538D4E";
+  let isAnswer = true;
   //match
   for (let i = 0; i < input.length; i++) {
     if (today[i] == input[i]) {
-      cur[i].style.backgroundColor = MATCH_BG_COLOR
-      today[i] = ""
-      input[i] = ""
-      continue
+      cur[i].style.backgroundColor = MATCH_BG_COLOR;
+      today[i] = "";
+      input[i] = "";
+      continue;
     }
-    isAnswer = false
+    isAnswer = false;
   }
 
-  return isAnswer
+  return isAnswer;
 }
 
 //자리 일치 검사
 function checkExist(input, today, cur) {
-  let EXIST_BG_COLOR = "#B59F3B"
+  let EXIST_BG_COLOR = "#B59F3B";
   //match
   for (let i = 0; i < input.length; i++) {
     for (let j = 0; j < today.length; j++) {
       if (input[i] == "") {
-        break
+        break;
       }
 
       if (input[i] == today[j]) {
-        cur[i].style.backgroundColor = EXIST_BG_COLOR
-        today[j] = ""
-        input[i] = ""
-        break
+        cur[i].style.backgroundColor = EXIST_BG_COLOR;
+        today[j] = "";
+        input[i] = "";
+        break;
       }
     }
   }
@@ -195,29 +199,30 @@ function checkExist(input, today, cur) {
 //결과 화면 출력
 function viewResult(message, visible) {
   setTimeout(() => {
-    document.querySelector("#pop_info_1").style.display = visible
-    document.querySelector(".result").textContent = message
-    document.querySelector(".wordle").textContent = `[ ${TODAY_WORDLE} ]`
-  }, 500)
+    document.querySelector("#pop_info_1").style.display = visible;
+    document.querySelector(".result").textContent = message;
+    document.querySelector(".wordle").textContent = `[ ${TODAY_WORDLE} ]`;
+  }, 500);
 }
 
 //게임 데이터 초기화
 function restart() {
-  document.querySelector("#pop_info_1").style.display = "none"
-  CUR_ROW = 1
-  CUR_COLUMN = 0
-  isGameOver = false
-  isSuccess = false
+  document.querySelector("#pop_info_1").style.display = "none";
+  CUR_ROW = 1;
+  CUR_COLUMN = 0;
+  isGameOver = false;
+  isSuccess = false;
   //updateWord();
-  drawBoard()
+  drawBoard();
 }
 
 //서버에 요청해서 단어 받아오기
 //당연하지만 서버가 켜져있어야하고, 요청을 처리 할 수 있어야 한다.
 function updateWord() {
   // return fetch("http://localhost:8080/wordle/wod/get-wordle");
-  return fetch("https://www.loomdis.site/TodayWordle");
+  return fetch("https://www.asud.site/TodayWordle");
 }
+
 // function updateWord() {
 //   return fetch("http://localhost:8080/wordle/wod/get-wordle");
 //   .then((response) => response.json())
@@ -227,6 +232,8 @@ function updateWord() {
 //   });
 // }
 
-var KEYFRAMES = {
-  s: [{ transform: "scale(1)" }, { transform: "scale(1.4)" }, { transform: "scale(1)" }]
-};
+// const onClickPromise = async (element) => {
+//   console.time("프로미스!");
+//   await new Promise((resolve) => element.animate(() => resolve(1), 500)), // 1
+//     console.timeEnd("프로미스!");
+// };
