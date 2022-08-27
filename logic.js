510f3943c0e319b058a6925000df1ae5bab11997
kyw <ingngnin@gmail.com>
2022-08-28 03:22:17 +0900

    개발할때 쓸 서버 jar파일로 만들고 ReadMe 만듬

--- logic.js
+++ logic.js
@@ -220,6 +220,11 @@ function restart() {
 //당연하지만 서버가 켜져있어야하고, 요청을 처리 할 수 있어야 한다.
 function updateWord() {
   // return fetch("http://localhost:8080/wordle/wod/get-wordle");
+
+  const URL = window.location.href;
+  if (URL.indexOf("127") == -1 || URL.indexOf("localhost") == -1) {
+    return fetch("http://127.0.0.1:5000/TodayWordle");
+  }
   return fetch("https://www.loomdis.site/TodayWordle");
 }
 
